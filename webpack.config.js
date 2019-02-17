18179520dcaff0aa6bbdd2204c9702fa0480d0d1
daniel <devidra87@gmail.com>
2019-02-17 13:40:43 -0800

    add packages and set up basic files and components

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const webpack = require('webpack');
 const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
+
+    
   entry: [
     'react-hot-loader/patch',
     'webpack-dev-server/client?http://localhost:8080',
@@ -20,6 +23,14 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
 
+  devtool: '#source-map',
+
+  devServer: {
+    hot: true,
+    contentBase: resolve(__dirname, 'build'),
+    publicPath: '/'
+  },
+
   module: {
     rules: [
       {
@@ -28,12 +39,25 @@ module.exports = {
         exclude: /node_modules/,
         options: {
           presets: [
-            "es2015",
-            "react"
+            ["es2015", {"modules": false}],
+            "react",
+          ],
+          plugins: [
+            "react-hot-loader/babel"
           ]
         }
-      },
-    ],
-  }
+      }
+    ]
+  },
 
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HtmlWebpackPlugin({
+      template:'template.ejs',
+      appMountId: 'react-app-root',
+      title: 'React Help Queue',
+      filename: resolve(__dirname, "build", "index.html"),
+    }),
+  ]
 };
