06bd3197a5b44f3d6a1d6aee4b643e771a0873ef
daniel <devidra87@gmail.com>
2019-02-22 15:46:55 -0800

    add unique Ids to each project

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ProjectDetails from './ProjectDetails';
+import { v4 } from 'uuid';
 
 function ProjectList () {
   return (
@@ -28,24 +29,28 @@ const Projects = [
   {
     projectName: 'Doctor Api',
     info: 'Angular application that allows you to search through existing databse for doctors based on Names and Specialties',
-    link: 'https://github.com/DanielL87/DoctorsApi.Solution.git'
+    link: 'https://github.com/DanielL87/DoctorsApi.Solution.git',
+    id: v4()
   },
   {
     projectName: 'Chat App',
     info: 'Angular application using FireBase to allow multiple users to chat with eachother in RealTime',
-    link: 'https://github.com/DanielL87/chat-app.git'
+    link: 'https://github.com/DanielL87/chat-app.git',
+    id: v4()
   },
 
   {
     projectName: 'Library Catalog',
     info: 'C# Application that allows users to enter both new clients, book stock and assign client to book relationships',
-    link: 'https://github.com/DanielL87/Library.Solution.git'
+    link: 'https://github.com/DanielL87/Library.Solution.git',
+    id: v4()
   },
 
   {
     projectName: 'Store Finder',
     info: 'C# Application that locates dispensaries based on users location, will also allow new stores to be added to database via built in web scraper',
-    link: 'https://github.com/DanielL87/StoreFinder-.git'
+    link: 'https://github.com/DanielL87/StoreFinder-.git',
+    id: v4()
   },
 
 ];
