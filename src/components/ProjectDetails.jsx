7e12ce38a82617ba7ec9c68125efbeca6693bc9b
Guest User <Guest@epicodus-118.local>
2019-02-28 10:08:03 -0800

    got details to show correctly on click

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function ProjectDetails (props) {
-  if (props.chosenProject == null){
+  if(props.selectedId== props.id){
+    return(
+      <div>
+      {console.log(props.chosenProject)}
+      <style jsx>{`
+      h4{
+      color: blue;    
+      }
+      `}
+      </style>
+      <div className='projectDetails'>
+        <h4  onClick={() => {props.selectProject(props.id)}}>{props.projectName}</h4>
+        <p><strong>Description:</strong> {props.chosenProject.info}</p>
+        <p id='links'><a href={props.chosenProject.link}>Github Link</a></p>
+      </div>
+    </div>
+      )
+  
+}else {
   return (
 
     <div>
@@ -17,24 +35,7 @@ function ProjectDetails (props) {
     </div>
     </div>
   )
-}else if(props.selectedId == props.id){
 
-  return(
-  <div>
-  {console.log(props.chosenProject)}
-  <style jsx>{`
-  h4{
-  color: blue;    
-  }
-  `}
-  </style>
-  <div className='projectDetails'>
-    <h4  onClick={() => {props.selectProject(props.id)}}>{props.projectName}</h4>
-    <p><strong>Description:</strong> {props.chosenProject.info}</p>
-    <p id='links'><a href={props.chosenProject.link}>Github Link</a></p>
-  </div>
-</div>
-  )
 }
 }
 
